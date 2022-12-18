1f606c5f1ba40f2429eb8b4324f53a14c39d2bdb
Amit Tripathi <tripathiamit2021@gmail.com>
2022-12-18 11:12:20 +0530

    created frontend

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './Components/Navbar';
+const App = () =>{
+  return(
+    <>
+        <Navbar/>
+    </>
+  )
+}
+
+export default App;
